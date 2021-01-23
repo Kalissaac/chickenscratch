f8325e31a14ae9c3dc1e432e9d17f6879b33dec3
Kalissaac <kalissaac@protonmail.com>
2021-01-23 15:04:53 -0800

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,6 +34,10 @@ function App ({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>Parchment</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width, height=device-height' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
       </Head>
 
       <ToastProvider autoDismiss components={{ Toast: Toast }} placement='bottom-right'>
