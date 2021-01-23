c203c223f3a20e067da0e60cef35d2421998c036
Kalissaac <kalissaac@protonmail.com>
2021-01-22 18:12:07 -0800

    Add dark mode to sidebar

--- components/document/sidebar.tsx
+++ components/document/sidebar.tsx
@@ -60,14 +60,14 @@ export default function DocumentSidebar ({ setSidebarOpen, sidebarOpen }: { setS
                     To: 'opacity-0'
                 */}
                 <div className='absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4'>
-                  <button aria-label='Close panel' onClick={() => setSidebarOpen(!sidebarOpen)} className='text-gray-800 hover:text-gray-500 text-3xl transition ease-in-out duration-150'>
+                  <button aria-label='Close panel' onClick={() => setSidebarOpen(!sidebarOpen)} className='text-gray-800 dark:text-gray-100 hover:text-gray-500 dark:hover:text-gray-400 text-3xl transition ease-in-out duration-150'>
                     {/* Heroicon name: x */}
                     <ion-icon name='close-outline' />
                   </button>
                 </div>
-                <div className='h-full flex flex-col space-y-6 py-6 bg-white shadow-xl overflow-y-scroll'>
+                <div className='h-full flex flex-col space-y-6 py-6 bg-white dark:bg-gray-900 shadow-xl overflow-y-scroll'>
                   <header className='px-4 sm:px-6'>
-                    <h2 className='text-lg leading-7 font-medium text-gray-900'>
+                    <h2 className='text-lg leading-7 font-medium text-gray-900 dark:text-gray-50'>
                       Preferences
                     </h2>
                   </header>
--- components/document/titlebar.tsx
+++ components/document/titlebar.tsx
@@ -9,9 +9,9 @@ export default function DocumentTitlebar ({ setSidebarOpen }: { setSidebarOpen: 
   }, [])
 
   return (
-    <div className='p-6 flex justify-between items-center text-gray-800 text-2xl bg-white opacity-80' style={{ backdropFilter: 'blur(24px)' }}>
+    <div className='p-6 flex justify-between items-center text-gray-800 dark:text-gray-50 text-2xl bg-white dark:bg-gray-900 opacity-80' style={{ backdropFilter: 'blur(24px)' }}>
       <button className='flex justify-center items-center' onClick={() => router.back()}><ion-icon name='chevron-back-outline' /></button>
-      <input id='doctitle' type='text' className='text-center font-serif outline-none focus:border-gray-800 border-transparent border-b-2 transition-all' placeholder='Untitled Document' />
+      <input id='doctitle' type='text' className='text-center font-serif outline-none bg-transparent focus:border-gray-800 dark:focus:border-gray-50 border-transparent border-b-2 transition-all' placeholder='Untitled Document' />
       {/* <div className='font-serif'>Title (#{pid})</div> */}
       <button className='flex justify-center items-center' onClick={() => setSidebarOpen(true)}><ion-icon name='information-circle-outline' /></button>
     </div>
