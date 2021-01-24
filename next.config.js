4a34b35cbedd07fb16199cd209e7d5d2d299e77e
Kalissaac <kalissaac@protonmail.com>
2021-01-24 10:06:44 -0800

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   async redirects () {
     return [
@@ -9,4 +13,4 @@ module.exports = {
       }
     ]
   }
-}
+})
