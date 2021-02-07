d170c5bcbde09df9d2d1ef7f577722e34b3eb1bd
Kalissaac <kalissaac@protonmail.com>
2021-02-06 18:44:19 -0800

    Add auto-resizing input field

--- src/components/document/titlebar.tsx
+++ src/components/document/titlebar.tsx
@@ -6,14 +6,18 @@ export default function DocumentTitlebar ({ setSidebarOpen }: { setSidebarOpen: 
   const router = useRouter()
 
   useEffect(() => {
-    document.getElementById('doctitle')?.setAttribute('value', `Title (#${router.query.document as string})`)
+    const input = document.getElementById('doctitle') as HTMLInputElement
+    if (input !== null) {
+      input.setAttribute('value', `Title (#${router.query.document as string})`)
+      input.size = Math.max(input?.value.length, 10)
+    }
   }, [router.query])
 
   return (
     <div className='p-6 flex justify-between items-center text-gray-800 dark:text-gray-50 text-2xl bg-white dark:bg-gray-900 opacity-80' style={{ backdropFilter: 'blur(24px)' }}>
-      <button className='flex justify-center items-center' onClick={() => router.back()}><ChevronLeft /></button>
-      <input id='doctitle' type='text' className='text-center font-serif outline-none bg-transparent focus:border-gray-800 dark:focus:border-gray-50 border-transparent border-b-2 transition-all' placeholder='Untitled Document' />
-      <button className='flex justify-center items-center' onClick={() => setSidebarOpen(true)}><Info /></button>
+      <button className='self-stretch flex justify-center items-center' onClick={() => router.back()}><ChevronLeft /></button>
+      <input id='doctitle' type='text' className='text-center font-serif outline-none bg-transparent focus:border-gray-800 dark:focus:border-gray-50 border-transparent border-b-2 transition-all' placeholder='Untitled Document' onChange={(e) => { e.currentTarget.size = Math.max(e.currentTarget.value.length, 10) }} />
+      <button className='self-stretch flex justify-center items-center' onClick={() => setSidebarOpen(true)}><Info /></button>
     </div>
   )
 }
