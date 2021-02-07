b7fac408acc748e35f4fa2f684cc11037800f84b
Kalissaac <kalissaac@protonmail.com>
2021-02-07 10:01:31 -0800

    Add table to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import InitialLoader from '@components/loader'
 import Nav from '@components/nav'
 import SearchBar from '@components/search'
 import { useUser } from '@shared/hooks'
-import { Plus } from '@kalissaac/react-feather'
+import { ChevronRight, FileText, Plus } from '@kalissaac/react-feather'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 
@@ -48,7 +48,7 @@ export default function HomePage (): JSX.Element {
           <button className={`text-xl uppercase transition-all ${activeTab === 'recentEdit' ? activeTabClasses : inactiveTabClasses}`} onClick={() => setActiveTab('recentEdit')}>Recently edited</button>
           <button className={`text-xl uppercase ml-5 transition-all ${activeTab === 'invitations' ? activeTabClasses : inactiveTabClasses}`} onClick={() => setActiveTab('invitations')}>Invitations</button>
         </div>
-        <div className='flex justify-between -ml-2 -mr-2'>
+        <div className='flex justify-between -ml-2 -mr-2 mb-12'>
           {activeTab === 'recentEdit' && Array(4).fill({}).map(() => (
             <Card key={Math.random() * 100}>
               <div>hi</div>
@@ -60,6 +60,69 @@ export default function HomePage (): JSX.Element {
             </Card>
           ))}
         </div>
+
+        <div className="flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Name
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Title
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Role
+                      </th>
+                      <th scope="col" className="relative px-6 py-3">
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    <tr>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="flex-shrink-0 h-10 w-10 flex justify-center items-center">
+                            <FileText className='w-1/2 h-1/2' />
+                          </div>
+                          <div className="ml-4">
+                            <div className="text-sm font-medium text-gray-900">
+                              Jane Cooper
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
+                        <div className="text-sm text-gray-500">Optimization</div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                          Active
+                        </span>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        Admin
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right font-medium">
+                        <a href="#" className="float-right"><ChevronRight /></a>
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
 
         <div className='py-96' />
       </div>
