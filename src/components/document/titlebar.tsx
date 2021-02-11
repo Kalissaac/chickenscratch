802f41469776abedf1efdae480c8f085dbf92d86
Kalissaac <kalissaac@protonmail.com>
2021-02-11 09:08:14 -0800

    Update navbar component

--- src/components/document/titlebar.tsx
+++ src/components/document/titlebar.tsx
@@ -16,7 +16,7 @@ export default function DocumentTitlebar ({ setSidebarOpen }: { setSidebarOpen: 
   return (
     <div className='p-6 flex justify-between items-center text-gray-800 dark:text-gray-50 text-2xl bg-white dark:bg-gray-900 opacity-80' style={{ backdropFilter: 'blur(24px)' }}>
       <button className='self-stretch flex justify-center items-center' onClick={() => router.back()}><ChevronLeft /></button>
-      <input id='doctitle' type='text' className='text-center font-serif outline-none bg-transparent focus:border-gray-800 dark:focus:border-gray-50 border-transparent border-b-2 transition-all' placeholder='Untitled Document' onChange={(e) => { e.currentTarget.size = Math.max(e.currentTarget.value.length, 10) }} />
+      <input id='doctitle' type='text' className='text-center font-serif outline-none bg-transparent focus:outline-none focus:border-gray-800 dark:focus:border-gray-50 border-transparent border-b-2 transition-all' placeholder='Untitled Document' onChange={(e) => { e.currentTarget.size = Math.max(e.currentTarget.value.length, 10) }} />
       <button className='self-stretch flex justify-center items-center' onClick={() => setSidebarOpen(true)}><Info /></button>
     </div>
   )
