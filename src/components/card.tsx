72dd3cfc28cb1ccd10e14eed500d8dac55e3da4f
Kalissaac <kalissaac@protonmail.com>
2021-02-12 14:42:20 -0800

    Update card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,13 +4,13 @@ import { ReactNode } from 'react'
 export default function Card ({ children }: { children?: ReactNode }): JSX.Element {
   const router = useRouter()
   return (
-    <div className='rounded-xl shadow-lg w-1/5 mx-2 bg-white dark:bg-gray-900 box-border overflow-hidden cursor-pointer transform-gpu transition-transform duration-200 hover:scale-105 border-2 focus:border-gray-darker'
+    <div className='rounded-xl shadow-lg flex-grow mx-2 bg-white dark:bg-gray-900 box-border overflow-hidden cursor-pointer transform-gpu transition-transform duration-200 hover:scale-105 focus:scale-105 border-2 border-transparent focus:border-gray-darker focus:outline-none'
       onClick={async () => await router.push(`/d/${Math.floor(Math.random() * 10000)}/edit`)} tabIndex={0}>
-      <div className='header p-6 py-4 text-gray-50 bg-gradient-to-r from-yellow-600 to-red-500'>
-        <div className='font-light'>subtitle</div>
-        <h2>Title</h2>
+      <div className='header p-4 text-gray-50 bg-gradient-to-r from-yellow-600 to-red-500'>
+        <div className='font-extralight text-xs'>last edited 2d ago &#8226; 354 words &#8226; due 23:59 Saturday</div>
+        <h2 className='text-lg'>Why is Water Wet?</h2>
       </div>
-      <div className='content p-6 text-sm'>
+      <div className='content p-4 text-sm'>
         {children}
       </div>
     </div>
