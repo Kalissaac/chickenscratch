081b147087ca4b0030118949a9d3c80a000958da
Kalissaac <kalissaac@protonmail.com>
2021-02-12 08:34:48 -0800

    Fix purge rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
-      './{components,pages}/**/*.{js,ts,jsx,tsx}'
+      './src/{components,pages}/**/*.{js,ts,jsx,tsx}'
     ]
   },
   darkMode: 'class', // 'media' or 'class'
