088799c72ae71474d1f82ba7efb7f70ea7e9281e
Kalissaac <kalissaac@protonmail.com>
2021-02-13 21:08:10 -0800

    Add proper document creation

--- src/pages/api/document/create.ts
+++ src/pages/api/document/create.ts
@@ -1,10 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import mongodb from 'mongodb'
+import { verifyTokenCookie } from '@shared/cookies'
+import { connectToDatabase } from '@shared/mongo'
 
 export default async function createDocument (req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const user = verifyTokenCookie(req.cookies.token)
+  const { client } = await connectToDatabase()
   try {
-    const documentID = new mongodb.ObjectID()
-    res.redirect(`/d/${documentID.toHexString()}/edit`)
+    const newFileRef = await client.db('data').collection('documents').insertOne({
+      title: '',
+      body: '',
+      lastModified: new Date(),
+      collaborators: [user.email]
+    })
+    res.redirect(`/d/${newFileRef.insertedId as string}/edit`)
     res.end()
   } catch (error) {
     res.status(500).json({ error: error.message })
