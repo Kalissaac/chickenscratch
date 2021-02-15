1f76a402149e064e2b7dcc03de4eb509eb230529
Kalissaac <kalissaac@protonmail.com>
2021-02-15 09:20:34 -0800

    Add custom error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import { Home } from '@kalissaac/react-feather'
+import Link from 'next/link'
+
+export default function NotFoundPage (): JSX.Element {
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center h-screen'>
+        <h1 className='font-bold font-serif text-4xl mb-4'>Oh snap!</h1>
+        <h3 className='font-medium text-lg mb-8'>We couldn't find the page you requested.</h3>
+        <Link href='/home'><a className='flex items-center rounded-lg bg-accent-1-500 p-4 py-3 shadow-xl hover:shadow-2xl transition-shadow text-gray-50'><Home className='mr-2' /> Go Home</a></Link>
+      </div>
+      <p className='uppercase font-serif font-bold text-lg border-black dark:border-white text-center -mt-12'>Parchment</p>
+    </>
+  )
+}
---
+++ src/pages/_error.tsx
@@ -0,0 +1,21 @@
+import { Home } from '@kalissaac/react-feather'
+import Link from 'next/link'
+
+export default function ErrorPage ({ statusCode }): JSX.Element {
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center h-screen'>
+        <h1 className='font-bold font-serif text-4xl mb-4'>Oh snap!</h1>
+        <h3 className='font-medium text-lg mb-1'>We encountered an internal server error. Please try again in a bit.</h3>
+        <p className='text-gray-500 mb-8'><code>{statusCode} at {(new Date()).toISOString()}</code></p>
+        <Link href='/home'><a className='flex items-center rounded-lg bg-accent-1-500 p-4 py-3 shadow-xl hover:shadow-2xl transition-shadow text-gray-50'><Home className='mr-2' /> Go Home</a></Link>
+      </div>
+      <p className='uppercase font-serif font-bold text-lg border-black dark:border-white text-center -mt-12'>Parchment</p>
+    </>
+  )
+}
+
+ErrorPage.getInitialProps = ({ res, err }) => {
+  const statusCode = res?.statusCode ?? err?.statusCode ?? 404
+  return { statusCode }
+}
