8d8ae4b58f83fab471f0e61b2f0021fa469f639f
Kian <kalissaac@protonmail.com>
2021-02-16 18:04:19 +0000

    Update document editing to use SWR

--- src/pages/api/document/get.ts
+++ src/pages/api/document/get.ts
@@ -5,15 +5,45 @@ import type File from '@interfaces/file'
 import { ObjectId } from 'mongodb'
 
 export default async function GetDocument (req: NextApiRequest, res: NextApiResponse): Promise<void> {
-  const documentID = req.query.id as string
+  const error = new Error()
   try {
+    const documentID = req.query.id as string
+    if (!documentID) {
+      error.name = 'NO_ID_SPECIFIED'
+      error.message = 'No document ID was specified in the request'
+      throw error
+    }
+
     const user = await verifyTokenCookie(req.cookies.token)
+    if (!user) {
+      error.name = 'USER_NOT_AUTHENTICATED'
+      error.message = 'User email could not be decoded from JWT'
+      throw error
+    }
+
     const { client } = await connectToDatabase()
     const requestedDocument: File = await client.db('data').collection('documents').findOne({ _id: ObjectId.createFromHexString(documentID) })
-    if (!requestedDocument.collaborators.includes(user.email)) throw new Error('User not authorized or document doesn\'t exist!')
+    if (!requestedDocument) {
+      error.name = 'FILE_NOT_FOUND'
+      error.message = 'MongoDB failed to locate document with ID: ' + documentID
+      throw error
+    }
+    if (!requestedDocument.collaborators.includes(user.email)) {
+      error.name = 'FILE_NOT_FOUND'
+      error.message = 'User not authorized to access document with ID: ' + documentID
+      throw error
+    }
+
     res.status(200).json({ document: requestedDocument })
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: error.message })
+    switch (error.name) {
+      case 'NO_ID_SPECIFIED':
+        res.status(404).json({ error: error.name })
+        break
+      default:
+        res.status(500).json({ error: error.name })
+        break
+    }
   }
 }
--- src/pages/d/[document]/edit.tsx
+++ src/pages/d/[document]/edit.tsx
@@ -3,18 +3,41 @@ import Titlebar from '@components/document/titlebar'
 import Sidebar from '@components/document/sidebar'
 import Editor from '@components/document/editor'
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
-import { connectToDatabase } from '@shared/mongo'
-import { ObjectId } from 'mongodb'
+import useSWR from 'swr'
+import { useUser } from '@shared/hooks'
+import { useRouter } from 'next/router'
+import InitialLoader from '@components/loader'
 
-export default function DocumentEditPage (props): JSX.Element {
-  const activeDocument = JSON.parse(props.activeDocument)
+export default function DocumentEditPage (): JSX.Element {
+  const router = useRouter()
+  const { user, loading: userLoading, error: userError } = useUser()
+  const { data: pageData, error: dataError } = useSWR(user ? `/api/document/get?id=${router.query.document as string}` : null)
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const activeDocument = pageData?.document
+
+  if (userLoading) {
+    // Show skeleton loader instead
+    return <InitialLoader />
+  }
+  if (userError) {
+    if (userError.name === 'USER_NOT_AUTHENTICATED') return <InitialLoader />
+    if (userError.message === 'NetworkError when attempting to fetch resource.') return <InitialLoader message={'Reconnecting...'} />
+    throw userError
+  }
+  if (!activeDocument && !dataError) {
+    // Show skeleton loader instead
+    return <InitialLoader />
+  }
+  if (dataError) {
+    if (dataError.name === 'USER_NOT_AUTHENTICATED') return <InitialLoader />
+    if (dataError.message === 'NetworkError when attempting to fetch resource.') return <InitialLoader message={'Reconnecting...'} />
+    throw dataError
+  }
 
   return (
     <>
       <Head>
-        <title>{activeDocument.title !== '' ? activeDocument.title : 'Untitled Document'} | Parchment</title>
+        <title>{activeDocument.title || 'Untitled Document'} | Parchment</title>
       </Head>
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} activeDocument={activeDocument} />
       <Titlebar setSidebarOpen={setSidebarOpen} activeDocument={activeDocument} />
@@ -23,21 +46,4 @@ export default function DocumentEditPage (props): JSX.Element {
       </div>
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const { client } = await connectToDatabase()
-  try {
-    const requestedDocument = await client.db('data').collection('documents').findOne({ _id: ObjectId.createFromHexString(params?.document as string) })
-    if (!requestedDocument) throw new Error('Document not found')
-    return {
-      props: {
-        activeDocument: JSON.stringify(requestedDocument)
-      }
-    }
-  } catch (error) {
-    return {
-      notFound: true
-    }
-  }
 }
