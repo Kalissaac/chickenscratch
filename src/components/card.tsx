def19160642650618068594fb8c57ec79f2a291d
Kian <kalissaac@protonmail.com>
2021-02-21 18:18:23 +0000

    Various updates

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,7 +6,7 @@ import { Node } from 'slate'
 
 dayjs.extend(relativeTime)
 
-const serialize = (nodes: Node[]): any => {
+const serialize = (nodes: Node[]): string => {
   return nodes.map(n => Node.string(n)).join('\n')
 }
 
@@ -14,7 +14,7 @@ export default function Card ({ file }: { file: ParchmentDocument }): JSX.Elemen
   const serializedBody = typeof file.body === 'string' ? file.body : serialize(file.body)
   return (
     <Link href={`/d/${file._id}/edit`}>
-      <a className='rounded-xl shadow-lg flex-1 min-w-0 mx-2 bg-white dark:bg-gray-900 box-border overflow-hidden cursor-pointer transform-gpu transition-transform duration-200 hover:scale-105 focus:scale-105 border-2 border-transparent focus:border-gray-darker dark:focus:border-gray-lighter focus:outline-none'>
+      <a className='rounded-xl shadow-lg hover:shadow-xl focus:shadow-xl flex-1 min-w-0 mx-2 bg-white dark:bg-gray-900 box-border overflow-hidden cursor-pointer transform-gpu transition-transform duration-200 hover:scale-105 focus:scale-105 bg-clip-border border-2 border-transparent focus:border-gray-darker dark:focus:border-gray-lighter focus:outline-none' key={file._id}>
         <div className='p-4 text-gray-50 bg-gradient-to-r from-yellow-600 to-red-500'>
           <div className='font-extralight text-xs'>{dayjs().to(dayjs(file.lastModified))} &#8226; {serializedBody.split(' ').length} {serializedBody.split(' ').length === 1 ? 'word' : 'words'} {file.due && `&#8226; due ${dayjs().to(dayjs(file.due))}`}</div>
           <h2 className='text-lg whitespace-nowrap overflow-ellipsis overflow-hidden'>{file.title}</h2>
