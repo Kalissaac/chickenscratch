e82e604646ee61844a2010b2290fd415bab8863e
Kian <kalissaac@protonmail.com>
2021-02-25 05:30:46 +0000

    Fix prose for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,70 @@ module.exports = {
         'gray-darker': '#090909',
         'gray-lighter': '#F2F2F2',
         gray: colors.trueGray
-      }
+      },
+      typography: (theme) => ({
+        light: {
+          css: [
+            {
+              color: theme('colors.gray.400'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.300')
+              },
+              a: {
+                color: theme('colors.white')
+              },
+              strong: {
+                color: theme('colors.white')
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400')
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.600')
+              },
+              hr: {
+                borderColor: theme('colors.gray.200')
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.600')
+              },
+              h1: {
+                color: theme('colors.white')
+              },
+              h2: {
+                color: theme('colors.white')
+              },
+              h3: {
+                color: theme('colors.white')
+              },
+              h4: {
+                color: theme('colors.white')
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.400')
+              },
+              code: {
+                color: theme('colors.white')
+              },
+              'a code': {
+                color: theme('colors.white')
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800')
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400')
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600')
+              }
+            }
+          ]
+        }
+      })
     },
     fontFamily: {
       sans: [...defaultTheme.fontFamily.sans],
@@ -39,7 +102,9 @@ module.exports = {
     }
   },
   variants: {
-    extend: {}
+    extend: {
+      typography: ['dark']
+    }
   },
   plugins: [require('@tailwindcss/typography')]
 }
