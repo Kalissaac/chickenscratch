739a4a9ec12015b3af90ccb0d4a044fab36de498
Kian <kalissaac@protonmail.com>
2021-03-02 02:34:06 +0000

    Separate cloud document from local document state

--- src/components/document/editor.tsx
+++ src/components/document/editor.tsx
@@ -5,7 +5,6 @@ import { useCallback, useMemo, useState } from 'react'
 import useSWR, { mutate } from 'swr'
 import { createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
-import type { Node } from 'slate'
 import { withHistory } from 'slate-history'
 import { DocumentSkeleton } from '@components/skeleton'
 import withShortcuts from '@components/document/editor/withShortcuts'
@@ -20,7 +19,7 @@ enum EditorModes {
   Suggesting
 }
 
-export default function DocumentEditor ({ activeDocument, mode }: { activeDocument: ParchmentDocument, mode: EditorModes }): JSX.Element {
+export default function DocumentEditor ({ cloudDocument, mode }: { cloudDocument: ParchmentDocument, mode: EditorModes }): JSX.Element {
   const editor = useMemo(() =>
     withNoDoubleSpaces(
       withShortcuts(
@@ -33,34 +32,35 @@ export default function DocumentEditor ({ activeDocument, mode }: { activeDocume
     ),
   [])
   const renderElement = useCallback(props => <Element {...props} />, [])
-  const [value, setValue] = useState<Node[]>(activeDocument.body)
+  const [activeDocument, setActiveDocument] = useState(cloudDocument)
   const [lastUpdate, setLastUpdate] = useState(new Date(activeDocument.lastModified).getTime())
   const router = useRouter()
 
   async function saveDocument (mutateDocument = false): Promise<void> {
     const docTitle = document.getElementById('doctitle') as HTMLInputElement | null
-    const updatedDocument = {
-      ...activeDocument,
-      title: docTitle?.value || 'Untitled Document', // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
-      body: value
-    }
-    if (compareDocuments(activeDocument, updatedDocument)) return
+    setActiveDocument((prevDocument) => {
+      return {
+        ...prevDocument,
+        title: docTitle?.value || 'Untitled Document' // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
+      }
+    })
+    if (compareDocuments(cloudDocument, activeDocument)) return
     const res = await fetch('/api/document/update', {
       method: 'POST',
       body: JSON.stringify({
         id: activeDocument._id,
         document: {
-          title: updatedDocument.title,
-          body: updatedDocument.body
+          title: activeDocument.title,
+          body: activeDocument.body
         }
       })
     })
 
     if (!res.ok) {
       const data = await res.json()
-        throw new Error(data.error)
-      }
-    if (mutateDocument) mutate(`/api/document/get?id=${activeDocument._id}`, updatedDocument).catch(console.error)
+      throw new Error(data.error)
+    }
+    if (mutateDocument) mutate(`/api/document/get?id=${activeDocument._id}`, activeDocument).catch(console.error)
     setLastUpdate(Date.now())
   }
 
@@ -78,9 +78,14 @@ export default function DocumentEditor ({ activeDocument, mode }: { activeDocume
   return (
     <Slate
       editor={editor}
-      value={value}
+      value={activeDocument.body}
       onChange={value => {
-        setValue(value)
+        setActiveDocument((prevDocument) => {
+          return {
+            ...prevDocument,
+            body: value
+          }
+        })
 
         if (mode !== EditorModes.Editing) return
         if ((Date.now() - lastUpdate) > 5000) { // If database was last updated more than 5 seconds ago
@@ -96,7 +101,7 @@ export default function DocumentEditor ({ activeDocument, mode }: { activeDocume
         className='prose dark:prose-light min-h-screen'
         readOnly={mode !== EditorModes.Editing}
       />
-      <DocumentStatusBar lastUpdate={lastUpdate} />
+      <DocumentStatusBar activeDocument={activeDocument} lastUpdate={lastUpdate} />
     </Slate>
   )
 }
@@ -104,15 +109,13 @@ export default function DocumentEditor ({ activeDocument, mode }: { activeDocume
 export function EditorWrapper ({ mode }: { mode: EditorModes }): JSX.Element {
   const router = useRouter()
   const { data: pageData } = useSWR(`/api/document/get?id=${router.query.document as string}`)
-  const activeDocument: ParchmentDocument = pageData?.document
+  const cloudDocument: ParchmentDocument = pageData?.document
 
   return (
     <div className='p-32 pt-16 px-6 sm:px-32 md:px-64 lg:px-72 xl:px-96'>
-      {activeDocument &&
-        <DocumentEditor activeDocument={activeDocument} mode={mode} />
-      }
-      {!activeDocument &&
-        <DocumentSkeleton />
+      {cloudDocument
+        ? <DocumentEditor cloudDocument={cloudDocument} mode={mode} />
+        : <DocumentSkeleton />
       }
     </div>
   )
