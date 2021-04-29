bc7730be63c15349ad5fef5682f99f916be70a35
Kian <kalissaac@protonmail.com>
2021-04-29 04:48:18 +0000

    Add counter menu to statusbar

--- src/components/document/editor/statusbar.tsx
+++ src/components/document/editor/statusbar.tsx
@@ -1,20 +1,19 @@
-import { Save } from '@kalissaac/react-feather'
-import { ReactNode } from 'react'
+import { Clock, Save } from '@kalissaac/react-feather'
+import { ReactNode, useState } from 'react'
 import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
 import ParchmentDocument from '@interfaces/document'
 import { Node } from 'slate'
-
-dayjs.extend(relativeTime)
+import { Transition } from '@headlessui/react'
 
 const serialize = (nodes: Node[]): string => nodes.map(n => Node.string(n)).join('\n')
 
 export default function DocumentStatusBar ({ activeDocument, lastUpdate, updateLock, saveDocument }: { activeDocument: ParchmentDocument, lastUpdate: number, updateLock: boolean, saveDocument: () => void }): JSX.Element {
+  const [openMenu, setOpenMenu] = useState('')
   return (
     <div className='fixed left-0 bottom-0 right-0 text-gray-900 dark:text-gray-50 text-opacity-60 dark:text-opacity-60 hover:text-opacity-100 dark:hover:text-opacity-100 transition-opacity text-xs font-editor flex justify-between items-center h-6 px-8'>
       <section className='h-full space-x-1'>
-        <StatusBarItem onClick={() => {}}>{serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0} words</StatusBarItem>
-        <StatusBarItem onClick={() => {}}>English (U.S.)</StatusBarItem>
+        <StatusBarItem onClick={() => { setOpenMenu(openMenu === 'counter' ? '' : 'counter') }}><CounterMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />{serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0} words</StatusBarItem>
+        {/* <StatusBarItem onClick={() => {}}>English</StatusBarItem> */}
       </section>
       <section className='h-full space-x-1'>
         <StatusBarItem onClick={() => {}} title={new Date().toString()}>
@@ -33,6 +32,14 @@ export default function DocumentStatusBar ({ activeDocument, lastUpdate, updateL
 
 function StatusBarItem ({ children, onClick, title }: { children: ReactNode, onClick: () => void, title?: string }): JSX.Element {
   return (
-    <button onClick={onClick} title={title} className='hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors px-2 h-full'>{children}</button>
+    <button onClick={onClick} title={title} className='hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors px-2 h-full relative'>{children}</button>
+  )
+}
+
+function CounterMenu ({ openMenu, setOpenMenu }): JSX.Element {
+  return (
+    <Transition show={openMenu === 'counter'} onBlur={() => setOpenMenu('')} className='absolute left-0 right-0 bottom-24 -top-24 h-24 bg-gray-800'>
+      Words
+    </Transition>
   )
 }
