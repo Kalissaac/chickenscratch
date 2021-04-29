7b76a331f245febc92614bc06c7f965c570f5edc
Kian <kalissaac@protonmail.com>
2021-04-29 19:29:30 +0000

    Add page counter and read time to statusbar

--- src/components/document/editor/statusbar.tsx
+++ src/components/document/editor/statusbar.tsx
@@ -1,4 +1,4 @@
-import { Clock, Save } from '@kalissaac/react-feather'
+import { Clock, Save, Type } from '@kalissaac/react-feather'
 import { ReactNode, useState } from 'react'
 import dayjs from 'dayjs'
 import ParchmentDocument from '@interfaces/document'
@@ -12,7 +12,10 @@ export default function DocumentStatusBar ({ activeDocument, lastUpdate, updateL
   return (
     <div className='fixed left-0 bottom-0 right-0 text-gray-900 dark:text-gray-50 text-opacity-60 dark:text-opacity-60 hover:text-opacity-100 dark:hover:text-opacity-100 transition-opacity text-xs font-editor flex justify-between items-center h-6 px-8'>
       <section className='h-full space-x-1'>
-        <StatusBarItem onClick={() => { setOpenMenu(openMenu === 'counter' ? '' : 'counter') }}><CounterMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />{serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0} words</StatusBarItem>
+        <StatusBarItem onClick={() => { setOpenMenu(openMenu === 'counter' ? '' : 'counter') }}><CounterMenu openMenu={openMenu} setOpenMenu={setOpenMenu} /><Type className='inline' /></StatusBarItem>
+        <StatusBarItem onClick={() => {}}>{serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0} words</StatusBarItem>
+        <StatusBarItem onClick={() => {}}>{((serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0) / 250).toFixed(2)} pages</StatusBarItem>
+        <StatusBarItem onClick={() => {}}>{Math.ceil((serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0) / 265)} min</StatusBarItem>
         {/* <StatusBarItem onClick={() => {}}>English</StatusBarItem> */}
       </section>
       <section className='h-full space-x-1'>
@@ -38,7 +41,7 @@ function StatusBarItem ({ children, onClick, title }: { children: ReactNode, onC
 
 function CounterMenu ({ openMenu, setOpenMenu }): JSX.Element {
   return (
-    <Transition show={openMenu === 'counter'} onBlur={() => setOpenMenu('')} className='absolute left-0 right-0 bottom-24 -top-24 h-24 bg-gray-800'>
+    <Transition show={openMenu === 'counter'} onBlur={() => setOpenMenu('')} className='absolute left-0 right-0 bottom-24 -top-24 h-24 bg-gray-300 dark:bg-gray-800'>
       Words
     </Transition>
   )
