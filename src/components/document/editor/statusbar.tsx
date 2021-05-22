c482d5aa877fbdbcae9b52bae1e196d3a3dd1ca6
Kian <kalissaac@protonmail.com>
2021-05-22 19:52:43 +0000

    Update statusbar background

--- src/components/document/editor/statusbar.tsx
+++ src/components/document/editor/statusbar.tsx
@@ -1,5 +1,5 @@
 import { Clock, Save, Type } from '@kalissaac/react-feather'
-import { ReactNode, useState } from 'react'
+import { ReactNode, useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 import ParchmentDocument from '@interfaces/document'
 import { Node } from 'slate'
@@ -9,16 +9,17 @@ const serialize = (nodes: Node[]): string => nodes.map(n => Node.string(n)).join
 
 export default function DocumentStatusBar ({ activeDocument, lastUpdate, updateLock, saveDocument }: { activeDocument: ParchmentDocument, lastUpdate: number, updateLock: boolean, saveDocument: () => void }): JSX.Element {
   const [openMenu, setOpenMenu] = useState('')
+  const wordCount = useMemo(() => serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0, [activeDocument.body])
   return (
     <div className='fixed left-0 bottom-0 right-0 text-gray-900 dark:text-gray-50 text-opacity-60 dark:text-opacity-60 hover:text-opacity-100 dark:hover:text-opacity-100 transition-opacity text-xs font-editor flex justify-between items-center h-6 px-8'>
-      <section className='h-full space-x-1'>
+      <section className='h-full space-x-1 bg-gray-lighter dark:bg-gray-darker'>
         <StatusBarItem onClick={() => { setOpenMenu(openMenu === 'counter' ? '' : 'counter') }}><CounterMenu openMenu={openMenu} setOpenMenu={setOpenMenu} /><Type className='inline' /></StatusBarItem>
-        <StatusBarItem onClick={() => {}}>{serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0} words</StatusBarItem>
-        <StatusBarItem onClick={() => {}}>{((serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0) / 250).toFixed(2)} pages</StatusBarItem>
-        <StatusBarItem onClick={() => {}}>{Math.ceil((serialize(activeDocument.body).match(/[\w\d’'-]+/gi)?.length ?? 0) / 265)} min</StatusBarItem>
+        <StatusBarItem onClick={() => {}}>{wordCount} words</StatusBarItem>
+        <StatusBarItem onClick={() => {}}>{(wordCount / 250).toFixed(2)} pages</StatusBarItem>
+        <StatusBarItem onClick={() => {}}>{Math.ceil(wordCount / 265)} min</StatusBarItem>
         {/* <StatusBarItem onClick={() => {}}>English</StatusBarItem> */}
       </section>
-      <section className='h-full space-x-1'>
+      <section className='h-full space-x-1 bg-gray-lighter dark:bg-gray-darker'>
         <StatusBarItem onClick={() => {}} title={new Date().toString()}>
           <Clock className='inline' /> {dayjs().format('HH:mm')}
         </StatusBarItem>
