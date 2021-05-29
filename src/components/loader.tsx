1b6a0f7dd5be1ec6728968679eaf3c7dd687d7f3
Kian <kalissaac@protonmail.com>
2021-05-29 23:09:16 +0000

    Set loader underline color to accent blue

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,7 +3,7 @@ import { AlertTriangle } from '@kalissaac/react-feather'
 export default function InitialLoader ({ message }: { message?: string }): JSX.Element {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <div className='uppercase font-serif font-bold text-5xl 2xl:text-6xl border-black dark:border-white border-b-2 pb-4 animate-pulse'>Parchment</div>
+      <div className='uppercase font-serif font-bold text-5xl 2xl:text-6xl border-accent-1-500 border-b-2 pb-4 animate-pulse'>Parchment</div>
       {message &&
         <div className={'rounded-lg absolute bottom-6 right-6 p-4 mt-2 shadow-xl text-gray-50 text-base flex justify-center items-center bg-accent-1-500'}>
           <AlertTriangle />
