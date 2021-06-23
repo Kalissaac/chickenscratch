b661ac892994e3619d5e646012e0ce6cea2bc56e
Kian <kalissaac@protonmail.com>
2021-06-23 02:01:50 +0000

    Fix multiple space prevention

--- webapp/src/components/document/editor/withNoDoubleSpaces.ts
+++ webapp/src/components/document/editor/withNoDoubleSpaces.ts
@@ -13,10 +13,16 @@ const withNoDoubleSpaces = (editor: ReactEditor): ReactEditor => {
     if (text === ' ' && selection && Range.isCollapsed(selection)) {
       const { anchor } = selection
       const before = Editor.before(editor, anchor, { unit: 'character' }) ?? anchor
-      const range = { anchor, focus: before }
-      const beforeText = Editor.string(editor, range)
+      const rangeBefore = { anchor, focus: before }
+      const beforeText = Editor.string(editor, rangeBefore)
 
       if (beforeText === ' ') return
+
+      const after = Editor.after(editor, anchor, { unit: 'character' }) ?? anchor
+      const rangeAfter = { anchor, focus: after }
+      const afterText = Editor.string(editor, rangeAfter)
+
+      if (afterText === ' ') return
     }
 
     insertText(text)
