34d442dda0986dfadd475a0c75841e4d335d5a89
Kian <kalissaac@protonmail.com>
2021-06-26 23:16:06 +0000

    Remove document bodies in home page api Dramatically reduces response size

--- webapp/src/pages/api/home.ts
+++ webapp/src/pages/api/home.ts
@@ -22,7 +22,7 @@ export default async function GetHomepageData (req: NextApiRequest, res: NextApi
 
     const { client } = await connectToDatabase()
     const allFiles: ParchmentDocument[] = await client.db('data').collection('documents')
-      .find({ collaborators: { $elemMatch: { user: email } }, deleted: { $exists: false }, archived: false })
+      .find({ collaborators: { $elemMatch: { user: email } }, deleted: { $exists: false }, archived: false }, { projection: { body: 0 } })
       .collation({ locale: 'en' })
       .sort({ title: 1 })
       .toArray()
