f3b91f5a126c5c8780186ff865bb7855d90fbda4
Kian <kalissaac@protonmail.com>
2021-08-08 08:20:00 -0700

    Cleanup footer

--- webapp/src/components/footer.tsx
+++ webapp/src/components/footer.tsx
@@ -4,11 +4,9 @@ import Link from 'next/link'
 
 export default function Footer (): JSX.Element {
   return (
-    <footer className='w-full h-36 bg-gray-900 flex justify-center items-center p-12 text-gray-200 text-center mt-auto'>
-      <div>
-        <Link href='/'><a className='uppercase font-serif font-semibold text-2xl border-b border-transparent hover:border-gray-200 transition-colors'>Parchment</a></Link>
-        <div className='font-thin text-xs mt-2 text-gray-200'>Copyright {new Date().getFullYear()} Refactored Sporks. All rights reserved.</div>
-      </div>
+    <footer className='w-full h-36 bg-gray-900 flex flex-col justify-center items-center p-12 text-gray-200 text-center mt-auto'>
+      <Link href='/'><a className='uppercase font-serif font-semibold text-2xl border-b border-transparent hover:border-gray-200 transition-colors'>Parchment</a></Link>
+      <div className='font-thin text-xs mt-2 text-gray-200'>Copyright {new Date().getFullYear()} Refactored Sporks. All rights reserved.</div>
     </footer>
   )
 }
