8bb57971fc5aaf57b1046943b822457d36a52c06
Kalissaac <kalissaac@protonmail.com>
2021-10-07 16:53:31 -0700

    Add Preact to client production builds

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -13,5 +13,17 @@ module.exports = withBundleAnalyzer({
         permanent: true
       }
     ]
+  },
+  // Replace React with Preact in client production build
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+
+    return config
   }
 })
