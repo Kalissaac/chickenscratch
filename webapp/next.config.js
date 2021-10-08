545f4324d6eb4a12dbcde0f8efc87487fa975dde
Kalissaac <kalissaac@protonmail.com>
2021-10-08 16:15:06 -0700

    Revert "Add Preact to client production builds"
    This reverts commit 8bb57971fc5aaf57b1046943b822457d36a52c06.
    Adding Preact breaks slate because of the way it uses onBeforeInput in React, which isn't consistent with its implementation in Preact. Will look into adding this back later down the line when the issue is fixed.

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -13,17 +13,5 @@ module.exports = withBundleAnalyzer({
         permanent: true
       }
     ]
-  },
-  // Replace React with Preact in client production build
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
   }
 })
