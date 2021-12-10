e0928622670702db74d3efdec7a705127ebbcc25
Kalissaac <kalissaac@protonmail.com>
2021-12-10 15:54:08 -0800

    Add formatISOToLocalDate function

--- webapp/src/shared/util.ts
+++ webapp/src/shared/util.ts
@@ -5,3 +5,16 @@ export function toUpper (string: string): string {
     .map(word => word[0].toUpperCase() + word.substr(1))
     .join(' ')
 }
+
+function padTimeElement (rawTime: number | string): string {
+  return rawTime.toString().padStart(2, '0')
+}
+
+export function formatISOToLocalDate (isoDate: string, includeSeconds: boolean = true): string {
+  if (!isoDate) return isoDate
+
+  const date = new Date(isoDate)
+  const dateSection = [date.getFullYear(), date.getMonth() + 1, date.getDate()].map(t => padTimeElement(t)).join('-')
+  const timeSection = [date.getHours(), date.getMinutes()].concat(includeSeconds ? [date.getSeconds()] : []).map(t => padTimeElement(t)).join(':')
+  return `${dateSection}T${timeSection}`
+}
